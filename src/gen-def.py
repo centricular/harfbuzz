2323bf20071718432a42c605375592c5d734b38a
Tim-Philipp Müller <tim@centricular.com>
2018-11-12 19:41:39 +0000

    meson: generate .def file for MSVC symbol export on the fly
    Fix symbol export with MSVC when features are disabled,
    such as GLib. We need to generate the list of exported
    symbols on the fly to make sure we only export symbols
    that are actually available.
    
    Needs some minor modifications to the gen-def.py script:
     - accept header list also via command line args; we can't
       pass things to a configure_file() command via the environment
       in Meson.
     - strip any leading 'src/' from library filename. This might
       be there because in Meson the script might be called from
       the top-level directory and not the current source directory.
    
    Remove .def files again which had been checked in for earlier
    versions of the Meson port.

--- src/gen-def.py
+++ src/gen-def.py
@@ -5,7 +5,14 @@ from __future__ import print_function, division, absolute_import
 import io, os, re, sys
 
 headers_content = []
-for h in os.environ["headers"].split (' '):
+if 'headers' in os.environ.keys():
+	headers = os.environ["headers"].split (' ')
+elif len(sys.argv) > 2:
+	headers = sys.argv[1:] # with Meson we pass headers as command line args
+else:
+	headers = []
+
+for h in headers:
 	if h.endswith (".h"):
 		with io.open (h, encoding='utf-8') as f: headers_content.append (f.read ())
 
@@ -13,7 +20,7 @@ result = """EXPORTS
 %s
 LIBRARY lib%s-0.dll""" % (
 	"\n".join (sorted (re.findall (r"^hb_\w+(?= \()", "\n".join (headers_content), re.M))),
-	sys.argv[1].replace ('.def', '')
+	sys.argv[1].replace ('src/', '').replace ('.def', '')
 )
 
 with open (sys.argv[1], "w") as f: f.write (result)
